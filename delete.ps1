aab645a5d0c54aa3f3bab0dfedad8fc6370b891e
Jeffery Cook <Jeffery.Cook@q2ebanking.com>
2017-12-13 10:24:53 -0600

    refactored data.ps1 and locationData.ps1 added zip2.ps1

--- delete.ps1
+++ delete.ps1
@@ -2,10 +2,13 @@ function Delete-Files
 {
     echo "Hello, From Delete Program!"
     pause
-    ${pool_name} = "Proxy_QA_431_main"
+    #${pool_name} = "Proxy_QA_431_main"
+    $script:webappName = $script:qa_proxy[$script:webpool-1].name
+    echo $webappName
     ${cur_drv} = (get-location).Drive.Name
-    Remove-Item ${cur_drv}:\Q2\WWWROOT\${pool_name}\* -recurse -exclude *.config -Force
+    # Remove-Item ("D:\Q2\WWWROOT\$script:qa_proxy[$script:webpool-1].name\*") -recurse -exclude *.config -Force
+    Remove-Item ("D:\Q2\WWWROOT\$script:webappName\*") -recurse -exclude *.config -Force
     write-host work in progress...
-    pause
+    # pause
 
 }
